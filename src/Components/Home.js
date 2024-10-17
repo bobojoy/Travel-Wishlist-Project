d4159ff4ffeae3aeab1892739295272e7391f601
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-10-17 12:31:07 +0300

    added icons on the footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,20 +1,85 @@
+// import React from "react";
+// import NavBar from "./NavBar";
+// import DestinationList from "./DestinationList";
+// import FormComponent from "./Form";
+// import '../App.css';
+
+// function Home() {
+//   return (
+//     < div className="Home">
+    
+//       <header className="header">
+       
+//         <NavBar />
+//       </header>
+//       <main>
+//         <FormComponent/> 
+//         <h1 className="list">Existing Destinations</h1>  
+//         <DestinationList />
+//       </main>
+//       <footer className="bg-gray-200 py-footer-padding text-center">
+//         <div className="social-media">
+//           <ul className="list-none p-0 flex justify-center space-x-4">
+//             <li>
+//               <a
+//                 href="https://www.facebook.com"
+//                 target="-blank"
+//                 rel="noopener noreferrer"
+//                 className="text-gray-800 hover:text-facebook"
+//               >
+//                 Facebook
+//               </a>
+//             </li>
+
+//             <li>
+//               <a
+//                 href="https://twitter.com"
+//                 target="-blank"
+//                 rel="noopener noreferrer"
+//                 className="text-gray-800 hover:text-twitter"
+//               >
+//                 Twitter
+//               </a>
+//             </li>
+
+//             <li>
+//               <a
+//                 href="https://www.instagram.com"
+//                 target="-blank"
+//                 rel="noopener noreferrer"
+//                 className="text-gray-800 hover:text-instagram"
+//               >
+//                 Instagram
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </footer>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
 import React from "react";
 import NavBar from "./NavBar";
 import DestinationList from "./DestinationList";
 import FormComponent from "./Form";
-import '../App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Home() {
   return (
-    < div className="Home">
-    
-      <header className="header">
-       
+    <>
+      <header>
         <NavBar />
       </header>
       <main>
-        <FormComponent/> 
-        <h1 className="list">Existing Destinations</h1>  
+        <FormComponent />
         <DestinationList />
       </main>
       <footer className="bg-gray-200 py-footer-padding text-center">
@@ -27,6 +92,7 @@ function Home() {
                 rel="noopener noreferrer"
                 className="text-gray-800 hover:text-facebook"
               >
+                <FontAwesomeIcon icon={faFacebook} className="mr-2" />
                 Facebook
               </a>
             </li>
@@ -38,6 +104,7 @@ function Home() {
                 rel="noopener noreferrer"
                 className="text-gray-800 hover:text-twitter"
               >
+                <FontAwesomeIcon icon={faTwitter} className="mr-2" />
                 Twitter
               </a>
             </li>
@@ -49,14 +116,16 @@ function Home() {
                 rel="noopener noreferrer"
                 className="text-gray-800 hover:text-instagram"
               >
+                <FontAwesomeIcon icon={faInstagram} className="mr-2" />
                 Instagram
               </a>
             </li>
           </ul>
         </div>
       </footer>
-    </div>
+    </>
   );
 }
 
 export default Home;
+

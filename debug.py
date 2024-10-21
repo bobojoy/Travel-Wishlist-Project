ddeb42eadc16387b9237f7dff1b8df2fbd605a2b
benard <benardboiwo703@gmail.com>
2024-10-21 09:54:29 +0300

    Travel-Wishlist-Project

---
+++ debug.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from app import app
+from models import db, Destination, WishlistItem  
+
+if __name__ == '__main__':
+    with app.app_context():
+        
+        db.create_all()
+        import ipdb; ipdb.set_trace()

1994a78acc29197e0f1982f4c65b38daec49ee88
allan <4lwachira@gmail.com>
2024-10-21 07:13:08 +0300

    Configure database

---
+++ app.py
@@ -0,0 +1,9 @@
+from models import db, Destination, Attraction, DestinationAttraction
+from flask_migrate import Migrate
+from flask import Flask, request, jsonify, make_response
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'travel.db')}")
+

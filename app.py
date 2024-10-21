a98e436278b7569f8bd0d08ff3251f4faff64c41
allan <4lwachira@gmail.com>
2024-10-21 07:15:52 +0300

    Initialize extensions

--- app.py
+++ app.py
@@ -11,3 +11,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
